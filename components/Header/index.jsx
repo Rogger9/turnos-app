640e57826199cd3275b5843aa93dcbe6df54219f
Roger9 <gutierrez.rog12@gmail.com>
2022-03-22 10:35:44 -0300

    Added conditional render

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,12 +1,21 @@
 import LinkButton from 'components/LinkButton'
-import { ADMIN, USERS } from 'routes'
+import { useRouter } from 'next/router'
+import { ADMIN, HOME, USERS } from 'routes'
 import styles from './style.module.css'
 
 export default function Header () {
+	const { pathname } = useRouter()
+	const isHome = pathname === '/'
 	return (
 		<header className={styles.container}>
-			<LinkButton to={USERS} value='Users' />
-			<LinkButton to={ADMIN} value='Admin' />
+			{
+				isHome
+					? <>
+						<LinkButton to={USERS} value='Users' />
+						<LinkButton to={ADMIN} value='Admin' />
+					</>
+					: <LinkButton to={HOME} value='Back' />
+			}
 		</header>
 	)
 }
--- components/Header/index.test.jsx
+++ components/Header/index.test.jsx
@@ -2,13 +2,25 @@ import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Header from 'components/Header'
 
+const useRouter = jest.spyOn(require('next/router'), 'useRouter')
+
 describe('Header', () => {
-	it('renders links', () => {
+
+	it('links at home', () => {
+		useRouter.mockImplementationOnce(() => ({ pathname: '/' }))
 		render(<Header />)
 		const users = screen.getByText('Users')
 		const admin = screen.getByText('Admin')
 
 		expect(users).toBeInTheDocument()
 		expect(admin).toBeInTheDocument()
+	})
+
+	it('links in /users', () => {
+		useRouter.mockImplementationOnce(() => ({ pathname: '/users' }))
+		render(<Header />)
+		const back = screen.getByText('Back')
+
+		expect(back).toBeInTheDocument()
 	})
 })
