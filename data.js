576a64290177e08806aa9e2ada5bc3441c97a021
Roger9 <gutierrez.rog12@gmail.com>
2022-03-25 09:53:23 -0300

    Mock data

---
+++ data.js
@@ -0,0 +1,105 @@
+export const data = {
+	1: [
+		{
+			name: 'John',
+			contact: 'correo@correo.com',
+			schedule: 10
+		},
+		{
+			name: 'John',
+			contact: 'correo@correo.com',
+			schedule: 8
+		}
+	],
+	4: [
+		{
+			name: 'John',
+			contact: 'correo@correo.com',
+			schedule: 9
+		},
+		{
+			name: 'John',
+			contact: 'correo@correo.com',
+			schedule: 11
+		}
+	],
+	9: [
+		{
+			name: 'John',
+			contact: 'correo@correo.com',
+			schedule: 12
+		}
+	],
+	12: [
+		{
+			name: 'John',
+			contact: 'correo@correo.com',
+			schedule: 9
+		},
+		{
+			name: 'John',
+			contact: 'correo@correo.com',
+			schedule: 8
+		}
+	],
+	15: [
+		{
+			name: 'John',
+			contact: 'correo@correo.com',
+			schedule: 8
+		},
+		{
+			name: 'John',
+			contact: 'correo@correo.com',
+			schedule: 12
+		}
+	],
+	17: [
+		{
+			name: 'John',
+			contact: 'correo@correo.com',
+			schedule: 10
+		},
+		{
+			name: 'John',
+			contact: 'correo@correo.com',
+			schedule: 8
+		}
+	],
+	24: [
+		{
+			name: 'John',
+			contact: 'correo@correo.com',
+			schedule: 8
+		},
+		{
+			name: 'John',
+			contact: 'correo@correo.com',
+			schedule: 11
+		}
+	],
+	29: [
+		{
+			name: 'John',
+			contact: 'correo@correo.com',
+			schedule: 11
+		},
+		{
+			name: 'John',
+			contact: 'correo@correo.com',
+			schedule: 12
+		}
+	],
+	30: [
+		{
+			name: 'John',
+			contact: 'correo@correo.com',
+			schedule: 9
+		},
+		{
+			name: 'John',
+			contact: 'correo@correo.com',
+			schedule: 10
+		}
+	]
+}
