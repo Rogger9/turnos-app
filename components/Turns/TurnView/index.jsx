b48d3f2e96a5dc367134d3a23d3a9fee2e53db38
Roger9 <gutierrez.rog12@gmail.com>
2022-04-02 23:57:37 -0300

    Added opacity feacture

--- components/Turns/TurnView/index.jsx
+++ components/Turns/TurnView/index.jsx
@@ -1,18 +1,22 @@
 import { useRouter } from 'next/router'
 import dynamic from 'next/dynamic'
-import { container, contrast, btn, bold } from './style.module.css'
+import { container, contrast, btn, bold, opacity } from './style.module.css'
 
 const Button = dynamic(() => import('components/Button'))
 
 export default function TurnView ({ id, name, contact, schedule, handleClick }) {
 	const { pathname } = useRouter()
+	const currentTime = new Date().getHours()
+	const isPast = schedule < currentTime
 
 	return (
-		<section className={container}>
+		<section className={isPast ? container + ' ' + opacity : container}>
 			<span className={bold}>{name}</span>
 			<span>{contact}</span>
 			<span><span className={contrast}>{schedule}</span> hs</span>
-			{pathname === '/admin' && <Button value='Delete' label='Delete' specificStyle={btn} handleClick={() => handleClick(id)} />}
+			{pathname === '/admin' && (
+				<Button value='Delete' label='Delete' specificStyle={btn} handleClick={() => handleClick(id)} isDisable={isPast} />
+			)}
 		</section>
 	)
 }
--- components/Turns/TurnView/index.test.jsx
+++ components/Turns/TurnView/index.test.jsx
@@ -1,11 +1,11 @@
-import { render, screen, waitFor } from '@testing-library/react'
+import { getByText, render, screen, waitFor } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import TurnView from '.'
 
 const data = {
 	name: 'John',
 	contact: 'correo@correo.com',
-	hour: 8
+	schedule: 8
 }
 
 const useRouter = jest.spyOn(require('next/router'), 'useRouter')
@@ -33,5 +33,14 @@ describe('TurnView', () => {
 		expect(name).toBeInTheDocument()
 		expect(contact).toBeInTheDocument()
 		await waitFor(() => expect(button).toBeInTheDocument())
+	})
+
+	it('hour of turn is past', async () => {
+		useRouter.mockImplementationOnce(() => ({ pathname: '/admin' }))
+		data.schedule = new Date().getHours() - 1
+		render(<TurnView {...data} />)
+
+		const button = await screen.findByText('Delete')
+		await waitFor(() => expect(button).toBeDisabled())
 	})
 })
