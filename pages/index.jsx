f7a53b47252914e7879815bbd7067c5c20570403
Roger9 <gutierrez.rog12@gmail.com>
2022-04-06 22:05:50 -0300

    Added getStaticProps

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,20 @@
 import PageLayout from 'components/PageLayout'
 import styles from 'styles/Home.module.css'
 
-export default function Home () {
+export default function Home ({ data }) {
+	console.log(data)
 	return (
 		<PageLayout style={styles.container}>
 			<h1>Select a view to continue</h1>
 		</PageLayout>
 	)
+}
+
+export async function getStaticProps () {
+	const res = await fetch('http://localhost:3000/api/turns')
+	const data = await res.json()
+
+	return {
+		props: { data }
+	}
 }
